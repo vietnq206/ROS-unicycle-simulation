6660626dbfcc971fa3afb44e2109a26738f1d14b
Viet <nguyen.viet@nokia.com>
2021-12-12 11:21:43 +0100

    update

--- src/custom_msg/src/nodeMultiply.cpp
+++ src/custom_msg/src/nodeMultiply.cpp
@@ -8,7 +8,7 @@
  
 
 class Component{
-public:
+
   double time_in;
   double data;
   double time_prev;
@@ -17,54 +17,28 @@ public:
   std::string nodeName;
   ros::Publisher signal_pub;
   custom_msg::custom msgOut,msgInP1, msgInP2;
-  Component (ros::NodeHandle nodeIn){
-    msgInP1.time = -1;
-    msgInP1.data = 0;  
-    msgInP2.time = -1;
-    msgInP2.data = 0; 
-    nodeName = ros::this_node::getName();
-    nodeIn.getParam(ros::this_node::getName()+"/InPort1",InPort1);
-    nodeIn.getParam(ros::this_node::getName()+"/InPort2",InPort2);
-    nodeIn.getParam(ros::this_node::getName()+"/OutPort1",OutPort); 
-    
-  }
+public:
+  Component (ros::NodeHandle nodeIn);
+  ~Component();
+  std::string getNameInPort(int port);
+  void initPub(ros::NodeHandle nodeIn);
+  void signalCallback_Port1(const custom_msg::custom::ConstPtr& msg);
+  void signalCallback_Port2(const custom_msg::custom::ConstPtr& msg);;
+  void publishMsg();
 
-  void initPub(ros::NodeHandle nodeIn){
-    signal_pub = nodeIn.advertise<custom_msg::custom>(OutPort, buffSize);
-  }
-  void signalCallback_Port1(const custom_msg::custom::ConstPtr& msg){
-    if (msg->time ==0 || msgInP1.time != msg->time)
-      {        
-      msgInP1.time = msg->time;
-      std::cout<<" DAta from port 1:"<<msg->data<<" > "<<msg->time<<std::endl;
-      //msgOut.data = sin(msg->data);
-      //signal_pub.publish(msgOut);
-      }
-   // ros::spinOnce();
-  }
-    void signalCallback_Port2(const custom_msg::custom::ConstPtr& msg){
-    if (msg->time ==0 || msgInP2.time != msg->time)
-      {        
-      msgInP1.time = msg->time;
-      std::cout<<" DAta from port 2:"<<msg->data<<" > "<<msg->time<<std::endl;
-      //msgOut.data = sin(msg->data);
-      //signal_pub.publish(msgOut);
-      }
-   // ros::spinOnce();
-  }
+
 };
-
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "func_Sin"); 
+  ros::init(argc, argv, "func_multiply"); 
   ros::NodeHandle node;
 
   Component *comp = new Component(node);
 
   comp->initPub(node);
-  ros::Subscriber sub1 = node.subscribe(comp->InPort1,100, &Component::signalCallback_Port1,comp); 
-  ros::Subscriber sub2 = node.subscribe(comp->InPort2,100, &Component::signalCallback_Port2,comp); 
+  ros::Subscriber sub1 = node.subscribe(comp->getNameInPort(1),100, &Component::signalCallback_Port1,comp); 
+  ros::Subscriber sub2 = node.subscribe(comp->getNameInPort(2),100, &Component::signalCallback_Port2,comp); 
 	
   int LoopRate;
   double result;
@@ -72,12 +46,7 @@ int main(int argc, char **argv)
   ros::Rate loop_rate(LoopRate);
   while (ros::ok()) { 
     ros::spinOnce();
-    
-    if(comp->msgInP1.time ==  comp->msgInP2.time){
-     comp->msgOut.data = comp->msgInP1.data * comp->msgInP2.data;
-     signal_pub.publish(msgOut)   
-    }
-
+    comp->publishMsg();
     loop_rate.sleep();
     
 	}
@@ -86,3 +55,53 @@ int main(int argc, char **argv)
   return 0;
 }
  
+std::string Component::getNameInPort(int port){
+    if (port == 1) return InPort1;
+    if (port == 2) return InPort2;
+}
+Component::Component (ros::NodeHandle nodeIn){
+  msgInP1.time = -1;
+  msgInP1.data = 0;  
+  msgInP2.time = -1;
+  msgInP2.data = 0; 
+  nodeName = ros::this_node::getName();
+  nodeIn.getParam(ros::this_node::getName()+"/InPort1",InPort1);
+  nodeIn.getParam(ros::this_node::getName()+"/InPort2",InPort2);
+  nodeIn.getParam(ros::this_node::getName()+"/OutPort1",OutPort); 
+  
+} 
+void Component::initPub(ros::NodeHandle nodeIn){
+  signal_pub = nodeIn.advertise<custom_msg::custom>(OutPort, buffSize);
+}
+void Component::signalCallback_Port1(const custom_msg::custom::ConstPtr& msg){
+  if (msg->time ==0 || msgInP1.time != msg->time)
+    {        
+    msgInP1.time = msg->time;
+    msgInP1.data = msg->data;
+  //  std::cout<<" DAta from port 1:"<<msg->data<<" > "<<msg->time<<std::endl;
+
+    }
+
+}
+void Component::signalCallback_Port2(const custom_msg::custom::ConstPtr& msg){
+  if (msg->time ==0 || msgInP2.time != msg->time)
+    {        
+    msgInP2.time = msg->time;
+    msgInP2.data = msg->data;
+   // std::cout<<" DAta from port 2:"<<msg->data<<" > "<<msg->time<<std::endl;
+
+    }
+
+}
+void Component::publishMsg(){
+  if( msgInP1.time ==  msgInP2.time)
+  {
+      msgOut.time = msgInP1.time;
+      msgOut.data =  msgInP1.data *  msgInP2.data;
+    signal_pub.publish(msgOut);   
+  }
+}
+Component::~Component(){
+
+}
+
--- src/custom_msg/src/nodeWriteOut.cpp
+++ src/custom_msg/src/nodeWriteOut.cpp
@@ -4,11 +4,65 @@
 #include "std_msgs/String.h"
 #include "custom_msg/custom.h"
 #include <string.h>
+#include <iostream>
+#include <fstream>
+
+
+class WriteFile{ 
+std::ofstream myfile;
+ros::Publisher signal_pub ;
+public:
+  WriteFile(std::string nameFile,ros::NodeHandle node ){
+    myfile.open(nameFile);
+      if (!myfile.is_open()){ 
+        throw "Can not open file!";
+      }
+   signal_pub = node.advertise<custom_msg::custom>( "outWrite", 1000);
+  }
+  ~WriteFile(){
+    myfile.close();
+  }
+  void signalCallback(const custom_msg::custom::ConstPtr& msg){ 
+    
+    myfile << msg->time << " "<< msg->data <<"\n";
+    
+    signal_pub.publish(msg);
+  }
+};
+
+
+
+
+
+
 
 int main(int argc, char **argv)
 {
+  ros::init(argc, argv, "write_data"); 
+  ros::NodeHandle node;
+  
+  std::string Inport;
+  std::string FileName;
+  std::string nodeName = ros::this_node::getName();
+  node.getParam(ros::this_node::getName()+"/FileOutName",FileName);
+  node.getParam(ros::this_node::getName()+"/InPort1",Inport);
+
+    // WriteFile File(FileName); 
+    // ros::Subscriber sub = node.subscribe(Inport,100, &WriteFile::signalCallback, &File); 
+   
+  try {
+    WriteFile File(FileName,node); 
+    ros::Subscriber sub = node.subscribe(Inport,100, &WriteFile::signalCallback, &File); 
+
+    ros::spin(); 
  
 
+  } catch (const char* msg) {
+     std::cerr << msg << std::endl;
+  }
+ 
+ 
+  
   return 0;
 }
  
